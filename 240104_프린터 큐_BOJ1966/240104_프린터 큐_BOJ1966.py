1e0d13b2545f397b2b7ab408738d0f407aadf011
yulia <hyr513@naver.com>
2024-01-05 02:48:14 +0900

    240104 Algorithm

--- 240104_프린터 큐_BOJ1966/240104_프린터 큐_BOJ1966.py
+++ 240104_프린터 큐_BOJ1966/240104_프린터 큐_BOJ1966.py
@@ -3,23 +3,30 @@
 # input.txt 열기
 import sys
 sys.stdin = open('input.txt')
-input = sys.stdin.readline()
-
-T = int(input)
+input = sys.stdin.readline
 from collections import deque #Queue
-queue = deque()
 
+T = int(input())
 for i in range(T):
-    n,m =map(int,input.split()) # 문서의 개수, 현재 몇 번째
-    importance = list(map(int,input.split())) # 중요도/맨 왼쪽 0번째
-    importance_que = deque(importance)
-    if
+    n, m = map(int, input().split()) # 문서의 개수, 현재 몇 번째
+    lst = list(map(int, input().split())) # 중요도/맨 왼쪽 0번째
 
+    count = 0
 
-  #
-  # 119111
-  # 012345
-  # queue.popleft()
+    que = deque()
+    for i in range(n):
+        que.append([lst[i], i])
+
+    while True:
+        if que[0][0] >= max(que)[0]:
+            temp = que.popleft()
+            count += 1
+            if temp[1] == m:
+                break
+        else:
+            que.rotate(-1)
+    print(count)
+
 
 
 
