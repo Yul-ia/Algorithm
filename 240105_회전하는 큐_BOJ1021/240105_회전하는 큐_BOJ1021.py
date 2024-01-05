14b0532f19a94d9df8a4cdd79d81e3f245835f94
yulia <hyr513@naver.com>
2024-01-06 02:35:47 +0900

    240105 Algorithm

--- 240105_회전하는 큐_BOJ1021/240105_회전하는 큐_BOJ1021.py
+++ 240105_회전하는 큐_BOJ1021/240105_회전하는 큐_BOJ1021.py
@@ -5,14 +5,29 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-#queue
+
 from collections import  deque
 que = deque()
 
 n,m = map(int,input().split())
-l1 = list(map(int, input().split()))
+location = list(map(int, input().split())) #location
 for i in range(1, n+1):
     que.append(i)
-print(que)
 
+count = 0
+
+for j in location:
+    while True:
+        if que[0] == j:
+            que.popleft()
+            break
+        else:
+            if que.index(j) > (len(que)//2):
+                que.rotate(1) # 문제 3번수행
+                count +=1
+            elif que.index(j) <= (len(que)//2):
+                que.rotate(-1) # 문제 2번수행
+                count += 1
+
+print(count)
 
