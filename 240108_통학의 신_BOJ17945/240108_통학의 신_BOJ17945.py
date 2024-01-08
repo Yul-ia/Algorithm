94713c8c3e7fbddbcc942c60a8cab672d3848290
yulia <hyr513@naver.com>
2024-01-09 00:14:26 +0900

    240108 Algorithm

--- 240108_통학의 신_BOJ17945/240108_통학의 신_BOJ17945.py
+++ 240108_통학의 신_BOJ17945/240108_통학의 신_BOJ17945.py
@@ -11,12 +11,11 @@ A,B = map(int,input().split())
 T1 = -A + math.sqrt(A**2-B)
 T2 = -A - math.sqrt(A**2-B)
 lst =[]
-if A**2==B:
+if A**2==B: # 짝수 중근 판별
     print(T1)
 else:
-    lst.append(T1)
-    lst.append(T2)
+    lst.append(int(T1))
+    lst.append(int(T2))
     lst.sort()
 
 print(lst)
-
