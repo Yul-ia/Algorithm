4191ab3731ba97bd0b0900b9a77ee543750b7949
yulia <hyr513@naver.com>
2024-01-16 01:46:05 +0900

    240108 Algorithm

--- 240108_통학의 신_BOJ17945/240108_통학의 신_BOJ17945.py
+++ 240108_통학의 신_BOJ17945/240108_통학의 신_BOJ17945.py
@@ -1,21 +1,24 @@
 # 통학의 신_BOJ17945
 
 # input.txt 열기
-import math
+# import math
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 A,B = map(int,input().split())
 
-T1 = -A + math.sqrt(A**2-B)
-T2 = -A - math.sqrt(A**2-B)
+T1 = -A + (A**2-B)**0.5
+T2 = -A - (A**2-B)**0.5
+
+
 lst =[]
 if A**2==B: # 짝수 중근 판별
-    print(T1)
+    print(int(T1))
+
 else:
     lst.append(int(T1))
     lst.append(int(T2))
     lst.sort()
 
-print(lst)
+    print(*lst)
