215ab27d270f2362a39d9e7e29ca768cd9fccb13
Yul-ia <hyr513@naver.com>
2024-02-28 03:05:14 +0900

    240227 Algorithm

--- 240227_나이계산하기_BOJ16199/240227_나이계산하기_BOJ16199.py
+++ 240227_나이계산하기_BOJ16199/240227_나이계산하기_BOJ16199.py
@@ -8,32 +8,15 @@ y2, m2, d2 = map(int,input().split())
 '''
 연 나이 = 세는 나이 -1
 '''
-if d2 > d1:
-    d3 = d2 - d1
-    if m2> m1 :
-        m3 = m2 - m1
-    else: # m2<m1
-        m3 = 12-m1+m2
+y_age = y2-y1 # 연 나이
+# 만 나이
+if m1>m2 and d1>d2:
+    print(y_age-1)
 else:
-    # 31일인 달 30일인달 28,29일달? d1> d2
-    d3 = 31-d1+d2
-    if m2> m1 :
-        m3 = m2 - m1
-    else: # m2<m1
-        m3 = 12-m1+m2
-print(m3,d3)
+    print(y_age)
 
-# 만나이
-if y2> y1 and m3>=m1 and d3 >= d1:
-    print(y2-y1+1) # 만나이
-    print(y2-y1+2) # 세는나이
-    print(y2-y1+1)
-elif y2>y1 and m3<m1 or d3 < d1:
-    print(y3-y1)
-    print(y3-y1+1)
-    print(y3-y1)
-else:
-    print(y2-y1) # 생일 안 지난 만
-    print(y2-y1+1) # 세는 나이
-    print(y2-y1) # 연나이
+# 세는 나이
+print((y_age+1)
 
+# 연 나이
+print(y_age)
