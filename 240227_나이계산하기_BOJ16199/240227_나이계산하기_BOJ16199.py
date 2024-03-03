7356679c8d52c482dbce35e637cbb4725afbf434
Yul-ia <hyr513@naver.com>
2024-03-03 23:27:54 +0900

    240227 Algorithm

--- 240227_나이계산하기_BOJ16199/240227_나이계산하기_BOJ16199.py
+++ 240227_나이계산하기_BOJ16199/240227_나이계산하기_BOJ16199.py
@@ -9,8 +9,10 @@ y2, m2, d2 = map(int,input().split())
 y_age = y2-y1 # 연 나이
 
 # 만 나이
-if m1>=m2 and d1>d2:
+if m1>m2:
     print(y_age-1) # 생일 안 지난 만 나이.
+elif m1 == m2 and d1>d2:
+    print(y_age - 1)
 else:
     print(y_age) # 생일 지난 만 나이
 
@@ -19,3 +21,11 @@ print(y_age+1)
 
 # 연 나이
 print(y_age)
+
+'''
+2000 04 04
+2001 03 04
+
+2000 04 04
+2001 04 03
+'''
